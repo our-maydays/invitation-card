fa42ebdc6047382d7cbdca63d99cdaff342dd67f
Gyeongbo Kang <gbkang@Gyeongboui-MacBookPro.local>
2026-02-17 20:46:31 +0900

    260217 20:46

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-	base: '/wedding-invitation/',
+	base: '/invitation-card/',
 	build: {
 		rollupOptions: {
 			output: {
